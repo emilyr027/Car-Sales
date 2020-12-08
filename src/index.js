96be4d92dd066ac2a10f749ed0ad0e54cefe1653
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2020-12-08 17:06:17 -0600

    completed mvp

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './components/reducers/reducer';
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, rootElement);
